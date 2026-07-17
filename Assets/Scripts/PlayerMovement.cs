5ea9b685c8ee1eb10ad6ce6ea1adc7ce23ace2b3
eeneskaplan <eneskpln25@gmail.com>
2026-07-17 21:53:08 +0300

    a

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Girdileri al (W, A, S, D)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Çapraz giderken hýzýn iki katýna çýkmasýný engelle
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
